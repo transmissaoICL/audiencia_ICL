9d19773ec52ebcd9a6d711a1459b97556323290e
transmissaoICL <leonardo.salles@icl.com.br>
2025-10-08 11:18:29 -0300

    colocando_abas

--- scripts/inline.js
+++ scripts/inline.js
@@ -71,6 +71,29 @@ programaDropdown.addEventListener('change', function() {
   programa = Number(programaDropdown.value);
 })
 
+function openPage(pageName, elmnt) {
+  // Hide all elements with class="tabcontent" by default */
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Remove the background color of all tablinks/buttons
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+  }
+
+  elmnt.style.backgroundColor = "#0056b3";
+  
+  // Show the specific tab content
+  document.getElementById(pageName).style.display = "block";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
 const alertMessage = document.getElementById("alertMessage");
 const alertCloseBtn = document.getElementById("closealert");
 
