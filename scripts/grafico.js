d8d1de75ecd043160f65ead03a429df7e9cf3194
transmissaoICL <leonardo.salles@icl.com.br>
2025-12-02 13:54:52 -0300

    grafico_local_funcionando

--- scripts/grafico.js
+++ scripts/grafico.js
@@ -48,52 +48,26 @@ const chart = new Chart(ctx, {
   }
 });
 
-async function fetchJSONData() {
-  const date = new Date(); // Data atualizada no momento da chamada
-  
-  // Tratamento para meses/dias menores que 10 (ex: 05 em vez de 5) se suas pastas usam zero à esquerda
-  // Se suas pastas são "2025/5/1", pode remover o .padStart
-  const year = date.getFullYear();
-  const month = date.getMonth() + 1; 
-  const day = date.getDate();
-  const dateStr = date.toISOString().split('T')[0];
-
-  const url = `../data/historicos/${year}/${month}/${day}/audiencia-${dateStr}-${periodos[periodo]}.json`;
-
-  try {
-      const response = await fetch(url);
-      if (!response.ok) throw new Error(`Erro HTTP: ${response.status}`);
-      return await response.json(); // Retorna o objeto JSON pronto
-  } catch (error) {
-      console.error('Falha ao buscar JSON:', error);
-      return null; // Retorna nulo em caso de erro para não quebrar o resto
-  }
-}
-
 async function atualizarGraficoAudiencia() {
 
-  let historico = await fetchJSONData();
+  let resposta = await fetch("/api/grafico", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({})
+  });
 
-  let historicoCanais = historico.audienciasCanais;
-  let historicoProgramas = historico.audienciasProgramas;
-
-  let timestampsDisponiveis = Object.keys(historicoCanais[0].dadosHistoricos);
-  timestampsDisponiveis.sort();
-  chart.data.labels = timestampsDisponiveis;
-
-
-  let historicoTotal = [...historicoCanais, ...historicoProgramas];
+  const data = resposta.json();
+  let historicoTotal = data.historicoTotal;
 
   chart.data.datasets = historicoTotal.map((canal, index) => {
-    console.log(canal);
     const dataPoints = timestampsDisponiveis.map(t => canal.dadosHistoricos[t] || 0);
     return {
-      label: `${canal.canal} (${canal.plataforma})`,
-      data: dataPoints,
-      borderWidth: 2,
-      fill: false,
-      tension: 0.2,
-      borderColor: coresPaleta[index % coresPaleta.length]
+    label: `${canal.canal} (${canal.plataforma})`,
+    data: dataPoints,
+    borderWidth: 2,
+    fill: false,
+    tension: 0.2,
+    borderColor: coresPaleta[index % coresPaleta.length]
     };
   });
 
